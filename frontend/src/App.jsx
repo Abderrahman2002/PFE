6596b26a5f9e92fed6daa307b4131415b891a9a0
youssef <youssefefkiren@gmail.com>
2025-03-07 15:58:30 +0000

    new commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './login';
+import Dashboard from './Dashboard';
+import ProtectedRoute from './ProtectedRoute';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
